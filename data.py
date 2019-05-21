ed3b02dbb5f7cf23b51e858db5757ffeee7c921d
wvelezmartin <wvelezmartin@gmail.com>
2019-05-21 21:51:47 +0200

    Starting to poke data and understand it

--- data.py
+++ data.py
@@ -8,3 +8,8 @@ import pandas as pd
 # Math and arrays
 import numpy as np
 
+# Open the CSV
+train = pd.read_csv("csv/train.csv")
+# See what's up
+print(train.columns)
+print(train["belongs_to_collection"][0])
